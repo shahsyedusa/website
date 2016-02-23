fee798d1c382f3d2b4a17e2c81358134024b6a05
Shah Syed <shah@shahsyed.com>
2016-02-23 01:30:11 -0500

    upgraded dependencies to latest versions, fixed custom.lessglitch (footer color for small viewports was different than larger ones)

--- app/main.component.js
+++ app/main.component.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core', 'angular2/platform/browser', 'angular2/router', './about/about.component', './about/header.component', './about/footer.component', './contact/contact.component', './projects/projects.component', './resume/resume.component'], function(exports_1) {
+System.register(['angular2/core', 'angular2/platform/browser', 'angular2/router', './about/about.component', './about/header.component', './about/footer.component', './contact/contact.component', './projects/projects.component', './resume/resume.component'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -76,7 +78,7 @@ System.register(['angular2/core', 'angular2/platform/browser', 'angular2/router'
                     __metadata('design:paramtypes', [])
                 ], MainComponent);
                 return MainComponent;
-            })();
+            }());
             exports_1("MainComponent", MainComponent);
             browser_1.bootstrap(MainComponent, [
                 router_1.ROUTER_PROVIDERS
--- app/projects/projects.component.js
+++ app/projects/projects.component.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core', 'angular2/router', './list/projectList/projectList.component', './list/chatserver/chatserver.component', './list/java-poker/java-poker.component', './list/joypad/joypad.component', './list/mappy-stars/mappy-stars.component', './list/reddit-clone/reddit-clone.component', './list/space-invaders/space-invaders.component', './list/torcom/torcom.component'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', './list/projectList/projectList.component', './list/chatserver/chatserver.component', './list/java-poker/java-poker.component', './list/joypad/joypad.component', './list/mappy-stars/mappy-stars.component', './list/reddit-clone/reddit-clone.component', './list/space-invaders/space-invaders.component', './list/torcom/torcom.component'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -98,7 +100,7 @@ System.register(['angular2/core', 'angular2/router', './list/projectList/project
                     __metadata('design:paramtypes', [])
                 ], ProjectsComponent);
                 return ProjectsComponent;
-            })();
+            }());
             exports_1("ProjectsComponent", ProjectsComponent);
         }
     }
