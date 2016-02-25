5e6653dc262f2c4096f9e2c0256636f45954a669
Shah Syed <shah@shahsyed.com>
2016-02-24 21:54:50 -0500

    updated navbar to be responsive, changed resume layout, added custom js script to index.html

---
+++ custom.js
@@ -0,0 +1,17 @@
+/*!
+ * custom.js
+ * Custom JS script for s37syed-website 1.0.0
+ * Written by Shah Syed
+ */
+
+$('body').delegate('.nav li a', 'click', function() {
+  var $thisLi = $(this).parents('li:first');
+  if (!$thisLi.hasClass('dropdown')) {
+    $('.navbar-collapse').collapse('hide'); 
+  }
+  var $ul = $thisLi.parents('ul:first');
+  if (!$thisLi.hasClass('active')){
+    $ul.find('li.active').removeClass('active');
+    $thisLi.addClass('active');       
+  }
+});
